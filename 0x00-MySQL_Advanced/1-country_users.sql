ea0fecc52d7b69188496ac5048a00049b1251159
Amir Alsayed <amyralsyd367@gmail.com>
2024-04-25 17:35:39 +0200

    task 0 and 1

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- create table users
+-- with id, email and name columns.
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') DEFAULT 'US'
+);
