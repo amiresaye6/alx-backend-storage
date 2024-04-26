5dc3ae13c4f40523ca561b7769e03b67bb1d52b4
Amir Alsayed <amyralsyd367@gmail.com>
2024-04-26 17:15:38 +0300

    /

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,17 @@
+-- Task 10 finally
+
+DELIMITER $$
+
+CREATE FUNCTION SafeDiv (a INT, b INT)
+RETURNS FLOAT
+BEGIN
+    DECLARE result FLOAT;
+    IF b = 0 THEN
+        SET result = 0;
+    ELSE
+        SET result = a / b;
+    END IF;
+    RETURN result;
+END;
+$$
+DELIMITER ;
