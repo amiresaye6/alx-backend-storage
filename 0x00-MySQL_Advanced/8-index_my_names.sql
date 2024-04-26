f46ebe5c9e66060d6258e4b174c752f666472e42
Amir Alsayed <amyralsyd367@gmail.com>
2024-04-26 16:39:47 +0300

    the speed of indexing, sliceing

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,4 +1,4 @@
 -- Context: Index is not the solution for any performance issue, but well used, it’s really powerful!
 
 CREATE INDEX idx_name_first
-ON names (SUBSTRING(name, 1, 1));
+ON names (name(1));
